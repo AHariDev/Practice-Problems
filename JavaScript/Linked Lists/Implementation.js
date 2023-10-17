ab1714a6ff307c64fa8f1cf3da6af21e15b5489c
Arun Hariharan <arunhariharan21@gmail.com>
2023-10-17 15:32:33 -0400

    Worked on insert() and remove() methods - few edge cases remaining

--- JavaScript/Linked Lists/Implementation.js
+++ JavaScript/Linked Lists/Implementation.js
@@ -26,13 +26,73 @@ class LinkedList {
         this.head = newNode;
         this.length++; 
     }
-    insert(index, value){
-        
+    printList(){
+        const array = []; 
+        let currNode = this.head; 
+        while(currNode){
+            array.push(currNode.value); 
+            currNode = currNode.next; 
+        }
+        return array; 
+    }
+    insert(index, value){ //Uses 0-based indexing
+        let currNode = this.head;
+        let insertNode = new Node(value);
+        let position = 0; 
+        if (index >= 0 && index <= this.length - 1){
+            if (index === 0){
+                this.prepend(value);
+                this.length++;
+            } else if (index === this.length){
+                this.append(value);
+            } else {
+                this.length++;
+                while(currNode){
+                    if (position === index-1){
+                        let tempNode = currNode.next;
+                        currNode.next = insertNode;
+                        insertNode.next = tempNode;
+                        break;
+                    }
+                    position++; 
+                    currNode = currNode.next;
+                }
+            }
+        }
+    }
+    // Note: Delete won't work because of garbage collection
+    remove(index){ //Uses 0-based indexing
+        let nodeToDelete; 
+        let currNode = this.head; 
+        let position = 0; 
+        if (index >= 0 && index <= this.length - 1){
+            if(index === 0){
+                this.head = this.head.next; 
+                this.length--; 
+            } else {
+                this.length--; 
+                while (currNode){
+                    if (position === index - 1){
+                        currNode.next = currNode.next.next;
+                        break;  
+                    }
+                    position++; 
+                    currNode = currNode.next; 
+                }
+            }
+        }
     }
 }
 
 const myLinkedList = new LinkedList(10);
+myLinkedList.insert(1, 4);
+myLinkedList.insert(1, 4);
+myLinkedList.remove(2);
+myLinkedList.remove(1);
 myLinkedList.append(5);
-myLinkedList.append(16);
-myLinkedList.prepend(1);
-console.log(myLinkedList);
+myLinkedList.insert(1, 4);
+myLinkedList.remove(2);
+myLinkedList.insert(1, 4);
+myLinkedList.insert(1, 4);
+console.log(myLinkedList.printList());
+console.log(myLinkedList.length);
