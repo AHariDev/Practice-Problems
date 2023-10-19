37a756b0efe0fc3e2d65ef232b6df9873695dd25
Arun Hariharan <arunhariharan21@gmail.com>
2023-10-19 14:47:58 -0400

    Checks if list is a palindrome - not most efficient solution

---
+++ JavaScript/Linked Lists/Palindrome.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var isPalindrome = function(head) {
+    var reverse = (str) => {
+        let join = ""; 
+        for (let i = str.length - 1; i >= 0; i--){
+            join += str[i]; 
+        }
+        return join; 
+    }
+    let arr = []; 
+    while(head){
+        arr.push(head.val.toString());
+        head = head.next; 
+    }
+    return (arr.join('') == reverse(arr.join('')));
+};
