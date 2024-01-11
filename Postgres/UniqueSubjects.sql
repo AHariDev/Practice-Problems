a16fa9ee4bd1065401a166585cdc05b871e33b7c
Arun Hariharan <arunhariharan21@gmail.com>
2024-01-10 20:36:20 -0500

    Number of Unique Subject Taught By Teacher

---
+++ Postgres/UniqueSubjects.sql
@@ -0,0 +1,10 @@
+-- Write your PostgreSQL query statement below
+
+-- Write a solution to calculate the number of unique subjects each teacher teaches in the university.
+-- Return the result table in any order.
+-- The result format is shown in the following example.
+
+SELECT teacher_id,  COUNT(DISTINCT subject_id) as cnt
+FROM Teacher
+GROUP BY teacher_id
+ORDER BY teacher_id;
